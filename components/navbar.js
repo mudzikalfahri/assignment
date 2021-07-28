d719c7563d9f937f8f354c646152910abd4e33d7
unknown <alfahrimudzik@gmail.com>
2021-07-28 20:38:53 +0700

    created configuration

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <nav className="fixed bg-white backdrop-filter backdrop-blur-lg bg-opacity-60 w-full border-b border-gray-300 md:border-0">
+          <div className="max-w-3xl mx-auto md:flex justify-between py-6 font-normal px-6 lg:px-0" >
+            <div className="text-md flex justify-center md:inline">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+              </svg>
+            </div>
+            <div className="w-3/4 md:w-1/3 mx-auto md:mx-0 mt-4 md:mt-0">
+              <ul className="text-md flex justify-between">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">About</a></li>              
+                <li><a href="#">Project</a></li>              
+              </ul>
+            </div>
+          </div>
+        </nav> 
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,31 @@
-import Head from 'next/head'
+import Navbar from "../components/navbar"
+
+export async function getServerSideProps(){
+  const res = await fetch('http://localhost:1337/posts');
+  const data = await res.json();
+  return {
+    props: {
+      data: data
+    }
+  }
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+    <div className="min-h-screen">
+        <Navbar/>
+        {/* body */}
+        <div className="max-w-3xl mx-auto px-6 pt-40 md:pt-28">
+          <div className="">
+            <h3 className="text-lg text-indigo-400 font-semibold flex items-center"> HELLO THERE,
+            </h3>
+            <h1 className="text-5xl font-bold my-4">I'm Mudzik Al Fahri</h1>
+            <p className="text-gray-500 text-md leading-loose">I'm from Yogyakarta, Indonesia. I’m a third year computer engineering student at Diponegoro University. I love to do some experiment with code. This website is one part of me on the internet
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </div>
+          
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
